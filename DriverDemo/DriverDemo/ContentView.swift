8a2c226f9283270d1c2d59a4c710269b4226bd85
Ammar Ahmed <58107422+Ammar478@users.noreply.github.com>
2024-03-02 08:54:20 +0300

    DriverDemo/1:initialize App

---
+++ DriverDemo/DriverDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DriverDemo
+//
+//  Created by Ammar Ahmed on 21/08/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DriverDemo/DriverDemo/DriverDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverDemoApp.swift
+//  DriverDemo
+//
+//  Created by Ammar Ahmed on 21/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct DriverDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
